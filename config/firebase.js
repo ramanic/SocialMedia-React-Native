272d329e1467607c22c0b848ee4524d98199529d
Raman Lamichhane <40167289+ramanic@users.noreply.github.com>
2022-03-22 20:13:22 +0545

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -2,14 +2,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyDLH0h3K_85Laamnjxzp-nKF5NDEjpT1f8',
-  authDomain: 'rn-insta-aaac4.firebaseapp.com',
-  projectId: 'rn-insta-aaac4',
-  storageBucket: 'rn-insta-aaac4.appspot.com',
-  messagingSenderId: '118072138844',
-  appId: '1:118072138844:web:e5ae69cc27e03c10719a99',
-};
+//firebaseConfig
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
